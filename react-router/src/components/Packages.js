d927e60eea727e57a3f20783c960d766a80a00c7
Bebe-Flo-So <120537192+Bebe-Flo-So@users.noreply.github.com>
2024-03-08 21:09:51 -0500

    updated App.js

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,17 +1,15 @@
+export default function Packages(props){
+    const displayPackages = props.packages.map((eachPackage) => <li>{eachPackage}</li>)
 
-
-export default function Packages({ packages }) {
-    const display = packages.map(p => <li>{p}</li>)
     return (
         <div>
             <div className="packages">
                 <div className="packagesHeader">
                     <h1>Our Packages</h1>
-                    <ul>
-                        {display}
-                    </ul>
                 </div>
-                
+                <ul>
+                    {displayPackages}
+                </ul>
             </div>
         </div>
     )
